d11d7c68d7134928e947bf16e8db6da2eeff7d54
Cristelos <cristelos@msn.com>
2025-04-15 19:19:58 +0200

    Adding pokemon page to list pokemons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,22 +4,26 @@ export const routes: Routes = [
   {
     path: '',
     redirectTo: 'about',
-    pathMatch: 'full'
+    pathMatch: 'full',
+  },
+  {
+    path: 'pokemons',
+    loadComponent: () => import('./pages/pokemons/pokemons-page.component'),
   },
   {
     path: 'about',
-    loadComponent: () => import('./pages/about/about-page.component')
+    loadComponent: () => import('./pages/about/about-page.component'),
   },
   {
     path: 'pricing',
-    loadComponent: () => import('./pages/pricing/pricing-page.component')
+    loadComponent: () => import('./pages/pricing/pricing-page.component'),
   },
   {
     path: 'contact',
-    loadComponent: () => import('./pages/contact/contact-page.component')
+    loadComponent: () => import('./pages/contact/contact-page.component'),
   },
   {
     path: '**',
-    redirectTo: 'about'
-  }
+    redirectTo: 'about',
+  },
 ];
